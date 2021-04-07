c30d1a7fdee06b91077e613f22ef3db5b4438ff5
baromatyo <baromatyo@gmail.com>
2021-04-07 13:57:12 +0200

    kész a második activity!!

--- app/src/main/java/com/example/covid_cilike/FeldolgozoActivity.java
+++ app/src/main/java/com/example/covid_cilike/FeldolgozoActivity.java
@@ -3,6 +3,7 @@ package com.example.covid_cilike;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class FeldolgozoActivity extends AppCompatActivity {
 
@@ -10,6 +11,12 @@ public class FeldolgozoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feldolgozo_acttivity);
+
+        EditText kiir = findViewById(R.id.kiir);
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            kiir.setText(extras.getString("nev")+" "+extras.getString("szuldat")+" ");
+        }
 
 
     }
